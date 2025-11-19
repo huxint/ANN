bef54e9b8db319db61975cdbe9ffd9b58537e159
huxint <1348168485@qq.com>
2025-11-19 22:20:20 +0800

    feat(network): Add implementation of neural network basic architecture.

---
+++ include/network.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <memory>
+#include <utility>
+#include <vector>
+#include <ranges>
+#include "layer.hpp"
+
+namespace nn {
+    class Network {
+    public:
+        Network() = default;
+
+        template <typename LayerType, typename... Args>
+        void addLayer(Args &&...args) {
+            layers_.emplace_back(std::make_unique<LayerType>(std::forward<Args>(args)...));
+        }
+
+        Vector forward(const Vector &input) {
+            Vector out = input;
+            for (auto &layer : layers_) {
+                out = layer->forward(out);
+            }
+            return out;
+        }
+
+        Vector predict(const Vector &input) {
+            return forward(input);
+        }
+
+        void backward(const Vector &output, double lr) {
+            Vector grad = output;
+            for (const auto &layer : layers_ | std::ranges::views::reverse) {
+                grad = layer->backward(grad, lr);
+            }
+        }
+
+    private:
+        std::vector<std::unique_ptr<Layer>> layers_;
+    };
+} // namespace nn
